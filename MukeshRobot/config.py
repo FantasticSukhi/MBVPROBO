e02cba4b1c436a9db61b4727fc764cd7d6611df7
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-01-04 19:06:21 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,20 +1,20 @@
 class Config(object):
     LOGGER = True
-    API_ID =None 
-    API_HASH = ""
-    TOKEN = ""  
-    OWNER_ID=None
+    API_ID =20353207 
+    API_HASH = "e5b2ac2f9c37bda345fa9fb5ade66961"
+    TOKEN = "7803412058:AAFLOVUwVwCxLSF0X0rtkKQQbv6zSCsz5wE"  
+    OWNER_ID=6713994904
     
-    SUPPORT_CHAT = "" 
-    START_IMG = ""
-    EVENT_LOGS = ()
-    MONGO_DB_URI= ""
+    SUPPORT_CHAT = "-1002124344872" 
+    START_IMG = "https://i.ibb.co/mTqzjHK/file-6723.jpg"
+    EVENT_LOGS = (-1002138809373)
+    MONGO_DB_URI= "mongodb+srv://SUKHI:starboy333@mamba.jb9uy.mongodb.net/?retryWrites=true&w=majority&appName=Mamba"
    
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://mcclbjwx:CqMrbec47cqL5KbaZOUDlVQWOscjNcKR@peanut.db.elephantsql.com/mcclbjwx"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""
+        "cpzwadqyiwbplccvissfnapimzloqwvl"
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "9LMMJQ30GM49"
 
     
     BL_CHATS = [] 
